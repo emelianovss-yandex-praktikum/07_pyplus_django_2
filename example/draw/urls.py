6a2765a6ca45a43842eedcb1c2c6235f972c02da
Sergey Emelyanov <karagabul@gmail.com>
2021-11-22 23:00:24 +0300

    Add models related

--- example/draw/urls.py
+++ example/draw/urls.py
@@ -1,9 +1,15 @@
-from django.urls import path
+from django.urls import path, include
 
 from draw import views
 
 app_name = 'draw'
 
+albums_urls = ([
+    path('', views.album_list, name='list'),
+    path('<int:pk>', views.album_detail, name='detail'),
+], 'albums')
+
 urlpatterns = [
-    path('foreign', views.foreign, name='foreign')
+    path('albums/', include(albums_urls)),
+    path('<int:pk>', views.draw_detail, name='detail')
 ]
--- example/draw/views.py
+++ example/draw/views.py
@@ -1,7 +1,31 @@
 from django.shortcuts import render
 
-from draw.models import Draw
+from draw.models import Draw, Album
+from django.contrib.auth import get_user_model
 
 
-def foreign(request):
-    return render(request, 'draw/foreign.html', {'objects': Draw.objects.all()})
+User = get_user_model()
+
+
+def album_list(request):
+    return render(
+        request,
+        'draw/album_list.html',
+        {'objects': User.objects.all()}
+    )
+
+
+def album_detail(request, pk):
+    return render(
+        request,
+        'draw/album_detail.html',
+        {'object': Album.objects.get(pk=pk)}
+    )
+
+
+def draw_detail(request, pk):
+    return render(
+        request,
+        'draw/draw_detail.html',
+        {'object': Draw.objects.get(pk=pk)}
+    )
